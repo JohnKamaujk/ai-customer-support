f12ce7bf9fcea0a4441a8f12e6471cd6a492ef77
JohnKamaujk <johnjkkamau@gmail.com>
2024-08-12 12:42:07 +0300

    :sparkles:chore: setup open ai

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from "next/server";
+import OpenAI from "openai";
+
+interface ChatMessage {
+  role: "system" | "user" | "assistant";
+  content: string;
+}
+
+const systemPrompt: string =
+  "You are a helpful AI customer support agent. Please assist the user with their queries.";
+
+export async function POST(req: NextRequest) {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY || "",
+  });
+
+  const data: ChatMessage[] = await req.json();
+
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: systemPrompt }, ...data],
+    model: "gpt-4",
+    stream: true,
+  });
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      const encoder = new TextEncoder();
+      try {
+        for await (const chunk of completion) {
+          const content = chunk.choices[0]?.delta?.content;
+          if (content) {
+            const text = encoder.encode(content);
+            controller.enqueue(text);
+          }
+        }
+      } catch (err) {
+        controller.error(err);
+      } finally {
+        controller.close();
+      }
+    },
+  });
+
+  return new NextResponse(stream);
+}
