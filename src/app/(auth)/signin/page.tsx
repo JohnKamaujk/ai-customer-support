527c713e8fc2c0f4281caf98827e5db45b906738
JohnKamaujk <johnjkkamau@gmail.com>
2024-08-14 17:28:32 +0300

    :bento:chore: Update signin UI

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -48,18 +48,20 @@ export default function Signin() {
 
   return (
     <Container
-      maxWidth="sm"
+      maxWidth={false}
       sx={{
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
         height: "100vh",
+        width: "100vw",
+        bgcolor: "var(--primary-bg)",
       }}
     >
       <Box
         sx={{
-          width: "100%",
+          width: "40%",
           p: 4,
           boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
           borderRadius: 2,
